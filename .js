88a949386bd8fe01c9f6829020e8fc23894527fa
ANISHA DUBEY <anishadubey18@gmail.com>
2022-07-15 01:21:31 +0530

    changes

--- .js
+++ .js
@@ -4,7 +4,7 @@
 
 <h2>What Can JavaScript Do?</h2>
 
-<p id="demo">JavaScript can change HTML content.</p>
+<p id="ABCD">JavaScript can change HTML content.</p>
 
 <button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
 
